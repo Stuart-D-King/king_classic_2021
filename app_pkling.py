7ea1eb8f6a4d5aa9913c86a34f9eef651befb467
Stuart King <stuartking@Stuarts-MacBook.local>
2021-07-31 23:00:14 -0400

    feat: increased high score to 12

--- app_pkling.py
+++ app_pkling.py
@@ -96,7 +96,7 @@ def enter_scores():
     players.sort()
 
     holes = [x for x in range(1,19)]
-    scores = [x for x in range(0,11)]
+    scores = [x for x in range(0,13)]
     course = 'None'
 
     if request.method == 'POST':
