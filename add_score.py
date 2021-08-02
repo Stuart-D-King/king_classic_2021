d03bc32d0036dd7f869800f1348886de5d4b0cb1
Stuart King <stuartking@Stuarts-MacBook.local>
2021-08-01 22:37:00 -0400

    feat: updated to include skins and net score dicts

--- add_score.py
+++ add_score.py
@@ -12,10 +12,12 @@ def add_score():
         with open('pkl_files/' + pf, 'rb') as f:
             golfer = pickle.load(f)
             golfer.scores['The National - Bluff/Ridge'] = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0}
+            golfer.skins_scores['The National - Bluff/Ridge'] = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0}
+            golfer.net_scores['The National - Bluff/Ridge'] = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0}
             with open('pkl_files/' + pf, 'wb') as f:
                 pickle.dump(golfer, f)
 
 
 if __name__ == '__main__':
     add_score()
-    print('Successfully added new score dict to all players')
+    print('Successfully added new score dicts to all players')
